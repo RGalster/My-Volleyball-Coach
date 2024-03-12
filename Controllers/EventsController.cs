02224d0734272e70910591adc665e5575e4dbc56
unknown <RichardAGalsterJr@gmail.com>
2024-03-12 13:09:13 -0400

    Initial commit for My-Volleyball-Coach

---
+++ Controllers/EventsController.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using YouthVolleyballHub.Data;
+using YouthVolleyballHub.Models;
+using YouthVolleyballHub.ViewModels;
+
+namespace YouthVolleyballHub.Controllers
+{
+    public class EventsController : Controller
+    {
+        private ApplicationDbContext context;
+
+        public EventsController(ApplicationDbContext dbContext)
+        {
+            context = dbContext;
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            List<Event> events = context.Events
+                .Include(e => e.Category)
+                .ToList();
+
+            return View(events);
+        }
+
+        [Authorize]
+        public IActionResult Add()
+        {
+
+            List<Organization> organizations = context.Organizations.ToList(); 
+            List<EventCategory> categories = context.Categories.ToList();
+            
+            AddEventViewModel addEventViewModel = new AddEventViewModel(organizations, categories);
+
+            
+            
+
+            return View(addEventViewModel);
+        }
+
+        [Authorize]
+        [HttpPost]
+        public IActionResult Add(AddEventViewModel addEventViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                EventCategory theCategory = context.Categories.Find(addEventViewModel.CategoryId);
+                Organization theOrganization = context.Organizations.Find(addEventViewModel.OrganizationId);
+
+                Event newEvent = new Event
+                {
+                    Name = addEventViewModel.Name,
+                    Description = addEventViewModel.Description,
+                    ContactEmail = addEventViewModel.ContactEmail,
+                    Organization = theOrganization,
+                    Category = theCategory
+                };
+
+                context.Events.Add(newEvent);
+                context.SaveChanges();
+
+                return Redirect("/Events");
+            }
+
+            return View(addEventViewModel);
+        }
+
+        [Authorize]
+        public IActionResult Delete()
+        {
+            ViewBag.events = context.Events.ToList();
+
+            return View();
+        }
+
+        [Authorize]
+        [HttpPost]
+        public IActionResult Delete(int[] eventIds)
+        {
+            foreach (int eventId in eventIds)
+            {
+                Event theEvent = context.Events.Find(eventId);
+                context.Events.Remove(theEvent);
+            }
+
+            context.SaveChanges();
+
+            return Redirect("/Events");
+        }
+
+        public IActionResult Detail(int id)
+        {
+            Event theEvent = context.Events
+               .Include(e => e.Category)
+               .Single(e => e.Id == id);
+
+            List<EventTag> eventTags = context.EventTags
+                .Where(et => et.EventId == id)
+                .Include(et => et.Tag)
+                .ToList();
+
+            EventDetailViewModel viewModel = new EventDetailViewModel(theEvent, eventTags);
+            return View(viewModel);
+
+        }
+    }
+}
---
+++ ViewModels/AddEventViewModel.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using YouthVolleyballHub.Models;
+
+namespace YouthVolleyballHub.ViewModels
+{
+    public class AddEventViewModel
+    {
+        [Required(ErrorMessage = "Name is required.")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Name must be between 3 and 50 characters")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Description is required")]
+        [StringLength(500, ErrorMessage = "Description too long!")]
+        public string Description { get; set; }
+
+        [EmailAddress]
+        public string ContactEmail { get; set; }
+
+        [Required(ErrorMessage = "Organization is required")]
+        public int OrganizationId { get; set; }
+        public List<SelectListItem> Organizations { get; set; }
+
+        [Required(ErrorMessage = "Category is required")]
+        public int CategoryId { get; set; }
+
+        public List<SelectListItem> Categories { get; set; }
+        public AddEventViewModel(List<Organization> organizations, List<EventCategory> categories)
+        {
+            Organizations = new List<SelectListItem>();
+            foreach (var organization in organizations)
+            {
+                Organizations.Add(
+                    new SelectListItem
+                    {
+                        Value = organization.Id.ToString(),
+                        Text = organization.Name
+                    }
+                ); ;
+            }
+
+            Categories = new List<SelectListItem>();
+            foreach (var category in categories)
+            {
+                Categories.Add(
+                    new SelectListItem
+                    {
+                        Value = category.Id.ToString(),
+                        Text = category.Name
+                    }
+                ); ;
+            }
+        }
+
+        
+
+        //public AddEventViewModel(List<EventCategory> categories)
+        //{
+        //    Categories = new List<SelectListItem>();
+
+        //    foreach (var category in categories)
+        //    {
+        //        Categories.Add(
+        //            new SelectListItem
+        //            {
+        //                Value = category.Id.ToString(),
+        //                Text = category.Name
+        //            }
+        //        ); ;
+        //    }
+        //}
+
+        public AddEventViewModel() 
+        { 
+        }
+    }
+}
